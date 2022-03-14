6e188124df593cc38733e792f75b06dda6459626
JayJay1024 <lygm1062@gmail.com>
2022-03-14 19:12:17 +0800

    feat: darwinia nft rewards remark

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,33 +1,20 @@
-import {SubstrateExtrinsic,SubstrateEvent,SubstrateBlock} from "@subql/types";
-import {StarterEntity} from "../types";
-import {Balance} from "@polkadot/types/interfaces";
+import {SubstrateExtrinsic} from "@subql/types";
+import {RemarkedNftAddress} from "../types";
 
+const nftRemarkPrefix = "NFT amount: 1; Rewards receiving address: 0x";
 
-export async function handleBlock(block: SubstrateBlock): Promise<void> {
-    //Create a new starterEntity with ID using block hash
-    let record = new StarterEntity(block.block.header.hash.toString());
-    //Record block number
-    record.field1 = block.block.header.number.toNumber();
-    await record.save();
+export async function handleSystemRemarkCall(extrinsic: SubstrateExtrinsic): Promise<void> {
+    const remark = (extrinsic.extrinsic.toHuman() as any)?.method?.args?.remark;
+    if (remark?.startsWith(nftRemarkPrefix)) {
+        const record = new RemarkedNftAddress(extrinsic.extrinsic.hash.toString());
+
+        record.signer = extrinsic.extrinsic.signer.toString();
+        record.value = remark;
+        record.addressValue = remark?.slice(nftRemarkPrefix.length - 2);  // TODO: Web3 util check address if valid or not
+        record.blockNumber = extrinsic.block.block.header.number.toNumber();
+        record.extrinsicIndex = extrinsic.idx;
+        record.extrinsicHash = extrinsic.extrinsic.hash.toString();
+        record.extrinsicTimestamp = extrinsic.block.timestamp;
+        await record.save();
+    }
 }
-
-export async function handleEvent(event: SubstrateEvent): Promise<void> {
-    const {event: {data: [account, balance]}} = event;
-    //Retrieve the record by its ID
-    const record = await StarterEntity.get(event.block.block.header.hash.toString());
-    record.field2 = account.toString();
-    //Big integer type Balance of a transfer event
-    record.field3 = (balance as Balance).toBigInt();
-    await record.save();
-}
-
-export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
-    const record = await StarterEntity.get(extrinsic.block.block.header.hash.toString());
-    //Date type timestamp
-    record.field4 = extrinsic.block.timestamp;
-    //Boolean tyep
-    record.field5 = true;
-    await record.save();
-}
-
-
